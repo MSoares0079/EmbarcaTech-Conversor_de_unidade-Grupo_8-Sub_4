896c2477b62cdb28fe2f1f3b6af9b0ef605014cc
MSoares0079 <matheus-ladeia@hotmail.com>
2024-12-26 16:36:20 -0300

    Adequação do código de " area.c " para os padrões.

--- funcoes/area.c
+++ funcoes/area.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
-void cm2_to_m2() {
+double cm2_to_m2() {
     double cm2, m2;
     printf("Digite a área em centímetros quadrados: ");
     scanf("%lf", &cm2);
     m2 = cm2 / 10000.0; // 1 m² = 10.000 cm²
-    printf("A área em metros quadrados é: %.4lf m²\n", m2);
+    return m2;
 }
 
-void m2_to_cm2() {
+double m2_to_cm2() {
     double m2, cm2;
     printf("Digite a área em metros quadrados: ");
     scanf("%lf", &m2);
     cm2 = m2 * 10000.0;
-    printf("A área em centímetros quadrados é: %.2lf cm²\n", cm2);
+    return cm2;
 }
 
-int main() {
+double area() {
     int escolha;
-    
+    double resultado;
     printf("Escolha o tipo de conversão:\n");
     printf("1 - Centímetros quadrados para metros quadrados\n");
     printf("2 - Metros quadrados para centímetros quadrados\n");
@@ -27,10 +27,10 @@ int main() {
     
     switch (escolha) {
         case 1:
-            cm2_to_m2();
+            resultado = cm2_to_m2();
             break;
         case 2:
-            m2_to_cm2();
+            resultado = m2_to_cm2();
             break;
         default:
             printf("Opção inválida.\n");
